ae6b185586dc6ade0e3441419b21bac9fe4f5c9c
orakovs <orakovs@gmail.com>
2024-02-05 16:30:01 +0600

    Slider and News

--- main/models.py
+++ main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+#sliders
+class Slider(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='slider')
+    link = models.URLField(max_length=256)
+
+    class Meta:
+        verbose_name = "Слайдер"
+        verbose_name_plural = "Слайдеры"
+
+    def __str__(self):
+        return self.name
+
+#news category
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=128)    
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Категория новостей"
+        verbose_name_plural = "Категории новостей"
+
+    def __str__(self):
+        return self.name
+
+#news
+class News(models.Model):
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='news')
+    datetime = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
-def Home(request):
+
+def Home(request, category_id=None):
+    sliders = Slider.objects.all()
+    selected_category = None
     
+    category_id = 1
+    selected_category = get_object_or_404(NewsCategory, pk=category_id)
+    
+    news = News.objects.filter(category=selected_category).order_by('-datetime')
     
     context = {
-        'current_page': 'home_url'
+        'current_page': 'home_url',
+        'sliders': sliders,
+        'news': news,
+        'selected_category': selected_category,
     }
     
     return render(request, 'home.html', context)
